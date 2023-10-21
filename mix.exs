6a3637b36330fa54898a9f47873fc101bad1d92c
Patrick Smith <pgwsmith@gmail.com>
2023-10-21 18:26:20 +1100

    Add mix production_build

--- mix.exs
+++ mix.exs
@@ -72,7 +72,12 @@ defmodule CollectedPublicData.MixProject do
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
+      production_build: [
+        "setup",
+        "assets.deploy",
+        "compile"
+      ]
     ]
   end
 end
